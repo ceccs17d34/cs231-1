bdc29d31fc8f2ea13c7f746fdba912d865954625
ceccs17d46 <43569233+ceccs17d46@users.noreply.github.com>
2018-11-09 18:50:14 +0530

    Add files via upload

---
+++ insertion_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int n,a[100],i,j,x;
+ 
+  printf("Enter number of elements:\n");
+  scanf("%d", &n);
+ 
+  printf("Enter integers:\n");
+ 
+  for (i= 0; i< n; i++)
+    scanf("%d",&a[i]);
+ 
+  for (i=1;i<=n-1;i++) {
+    j=i;
+ 
+    while(j>0 && a[j-1]>a[j]) {
+      x=a[j];
+      a[j]=a[j-1];
+      a[j-1] =x;
+ 
+      j--;
+    }
+  }
+ 
+  printf("Sorted list:\n");
+ 
+  for (i=0;i<=n-1;i++) {
+    printf("%d\n", a[i]);
+  }
+ 
+  return 0;
+}
